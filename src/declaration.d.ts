0d186d1b95634d5ea4fec7b8402cbd8c149f841e
Gustavo Muñoz <timbergus@gmail.com>
2020-05-16 17:18:02 +0200

    Add some examples.

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -8,3 +8,5 @@ declare module '*.gif';
 // Check this error. The original types are not working!!
 // @types/mini-css-extract-plugin
 declare module 'mini-css-extract-plugin';
+
+declare module 'recoil';
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
 import homeStyles from './home.css';
 
-const Home = () => (
-  <div className={homeStyles.container}>
-    <div data-testid="message">Hello World!</div>
-  </div>
-);
+interface Named {
+  name: string;
+  surname?: string | boolean;
+}
+
+interface GreetAble extends Named {
+  greet: (phrase: string) => void;
+}
+
+class Person implements GreetAble {
+  constructor(public name: string, public surname: boolean) {}
+  greet() {
+    console.log(`Hi ${this.name} ${this.surname}!`);
+  }
+}
+
+const person = new Person('John', true);
+
+person.greet();
+
+enum Role {
+  ADMIN,
+  USER,
+  AUTHOR,
+}
+
+const Home: FunctionComponent = () => {
+  let name = 'John';
+  let age = 35;
+  let single = true;
+
+  let mix: (string | number)[];
+
+  mix = ['John', 'Doe', 25114452];
+
+  console.log(Role.ADMIN);
+  console.log(Role.USER);
+  console.log(Role.AUTHOR);
+
+  const add = (a: number, b: number): number => a + b;
+
+  let myFunction: (a: number, b: number) => number;
+
+  myFunction = add;
+
+  console.log(myFunction(3, 2));
+
+  return (
+    <div className={homeStyles.container}>
+      <div data-testid="message">Hello World!</div>
+      <div>{name}</div>
+      <div>{age}</div>
+      <div>{single.toString()}</div>
+      <div>{`Full name: ${mix[0]} ${mix[1]} (${mix[2]})`}</div>
+    </div>
+  );
+};
 
 export default Home;
